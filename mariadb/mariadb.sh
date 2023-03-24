8b4c006ea1b11ac930883dc3f13faa6a288a6895
Mouad Mounib <mmounib@e1r1p7.1337.ma>
2023-03-24 19:51:16 +0000

    Need To Configure wp-config, also setuping the environment variables and finally configuring the volumes in the VMware

--- mariadb/mariadb.sh
+++ mariadb/mariadb.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 # Install MariaDB
-apt-get update
-apt-get install -y mariadb-server
+service mysql start
 
 # Start MariaDB service
-# service mysql start
 
 # Set root password
-# MYSQL_ROOT_PASSWORD=mysecretpassword
+# SQL_ROOT_PASSWORD=mysecretpassword
 
-# Create database and user
-# mysql -e "CREATE DATABASE mydatabase;"
-# mysql -e "CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypassword';"
-# mysql -e "GRANT ALL PRIVILEGES ON mydatabase.* TO 'myuser'@'localhost';"
+# # Create database and user
+mysql -e "CREATE DATABASE IF NOT EXISTS my_database;"
+mysql  -e "CREATE USER IF NOT EXISTS mmounib@'%' IDENTIFIED BY 'db_pass';"
+mysql  -e "GRANT ALL PRIVILEGES ON my_database.* TO mmounib@'%' IDENTIFIED BY '0123456789';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
 
-# Update bind-address to allow connections from any IP address
-# sed -i 's/bind-address\s*= 127.0.0.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+# # # # Update bind-address to allow connections from any IP address
+# # sed -i 's/bind-address\s*= 127.0.0.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+# service mysql start
 
-# Restart MariaDB service
-# service mysql restart
+# # Restart MariaDB service
 
-# Output success message
-mysqld_safe
+# # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# # mariadbd
 
+# # service mariadb start
+
+#tail -f
 # echo "MariaDB installation and configuration completed successfully!"
