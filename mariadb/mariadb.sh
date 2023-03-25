1f3087bcf34768e47ccf8c7325987d1759942608
Mouad Mounib <mmounib@e1r1p8.1337.ma>
2023-03-25 15:24:33 +0000

    Fixing The Issues, just need to install debian(ubuntu) in order to locate the volume

--- mariadb/mariadb.sh
+++ mariadb/mariadb.sh
@@ -9,21 +9,20 @@ service mysql start
 # SQL_ROOT_PASSWORD=mysecretpassword
 
 # # Create database and user
-mysql -e "CREATE DATABASE IF NOT EXISTS my_database;"
-mysql  -e "CREATE USER IF NOT EXISTS mmounib@'%' IDENTIFIED BY 'db_pass';"
-mysql  -e "GRANT ALL PRIVILEGES ON my_database.* TO mmounib@'%' IDENTIFIED BY '0123456789';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $db_name;"
+mysql -e "CREATE USER IF NOT EXISTS $db_user@'%' IDENTIFIED BY '$db_pass';"
+mysql -e "GRANT ALL PRIVILEGES ON \`$db_name\`.* TO '$db_user'@'%' IDENTIFIED BY '$db_pass';"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
 
 # # # # Update bind-address to allow connections from any IP address
 # # sed -i 's/bind-address\s*= 127.0.0.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 # service mysql start
 
 # # Restart MariaDB service
-
+service mysql stop
 # # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 # # mariadbd
-
+mysqld
 # # service mariadb start
-
 #tail -f
 # echo "MariaDB installation and configuration completed successfully!"
--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# sleep 10
+mkdir -p /var/www/html
+cd /var/www/html
+rm -rf *
+wp core download --allow-root
+
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 sed -i -r "s/database_name_here/$db_name/1" wp-config.php && sed -i -r "s/username_here/$db_user/1" wp-config.php
 sed -i -r "s/password_here/$db_pass/1" wp-config.php && sed -i -r "s/localhost/mariadb/1" wp-config.php
 
-wp core install --url=$DOMAIN_NAME/ --title=inception --admin_user=mouad --admin_password=234 --admin_email=mouad@dd.ss --skip-email --allow-root
+wp core install --url=localhost/ --title=inception --admin_user=mouad --admin_password=234 --admin_email=mouad@dd.ss --skip-email --allow-root
+wp user create user user@example.com --role=author --user_pass=user123 --allow-root
 
 php-fpm7.3 -F
