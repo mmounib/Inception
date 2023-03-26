ccf130f0fe5d05657822b32556f768d03df41261
Mouad Mounib <mmounib@student.1337.ma>
2023-03-26 11:53:02 +0000

    Fixing The Issues, just need to install debian(ubuntu) in order to locate the volume

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -6,7 +6,7 @@ service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS $db_name;"
 mysql -e "CREATE USER IF NOT EXISTS $db_user@'%' IDENTIFIED BY '$db_pass';"
 mysql -e "GRANT ALL PRIVILEGES ON \`$db_name\`.* TO '$db_user'@'%' IDENTIFIED BY '$db_pass';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
+# mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '123';"
 
 # Stop MariaDB service
 service mysql stop
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -10,7 +10,7 @@ mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sed -i -r "s/database_name_here/$db_name/1" wp-config.php && sed -i -r "s/username_here/$db_user/1" wp-config.php
 sed -i -r "s/password_here/$db_pass/1" wp-config.php && sed -i -r "s/localhost/mariadb/1" wp-config.php
 
-wp core install --url=localhost/ --title=inception --admin_user=mouad --admin_password=234 --admin_email=mouad@dd.ss --skip-email --allow-root
-wp user create user user@example.com --role=author --user_pass=user123 --allow-root
+wp core install --url=localhost/ --title=$wp_title --admin_user=$wp_admin --admin_password=$wp_pass --admin_email=mouad@dd.ss --skip-email --allow-root
+wp user create $reg_user user@example.com --role=author --user_pass=$reg_pass --allow-root
 
 php-fpm7.3 -F
