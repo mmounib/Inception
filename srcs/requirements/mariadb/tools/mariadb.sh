335b714c92d0889ef9a1d02a584d96d2d5a8f621
Mouad Mounib <mmounib@student.1337.ma>
2023-03-27 16:44:12 +0000

    everything is alright

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -2,13 +2,12 @@
 
 service mysql start
 
+
 # Create database and user
 mysql -e "CREATE DATABASE IF NOT EXISTS $db_name;"
 mysql -e "CREATE USER IF NOT EXISTS $db_user@'%' IDENTIFIED BY '$db_pass';"
 mysql -e "GRANT ALL PRIVILEGES ON \`$db_name\`.* TO '$db_user'@'%' IDENTIFIED BY '$db_pass';"
-# mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '123';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
 
 # Stop MariaDB service
-service mysql stop
-
-mysqld
+# service mysql stop
